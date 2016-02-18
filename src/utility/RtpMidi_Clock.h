f69f4e207b0b0e9ee31c7b74e1744ce43f97b3a6
lathoub <bart.de.lathouwer@telenet.be>
2016-02-18 22:24:03 +0100

    License consistency
    Changed License in code from GPL to Beerware (consistent with LICENSE.md
    file)

--- src/utility/RtpMidi_Clock.h
+++ src/utility/RtpMidi_Clock.h
@@ -5,7 +5,7 @@
  *	Version		0.3
  *  @author		lathoub 
  *	@date		27/06/14
- *  License		GPL
+ *  License		Code is open source so please feel free to do anything you want with it; you buy me a beer if you use this and we meet someday (Beerware license).
  */
 
 #pragma once
