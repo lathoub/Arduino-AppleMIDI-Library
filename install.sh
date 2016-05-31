e6524d5b8c78abfa1201070954ff3b01e5ede4b1
lathoub <bart.de.lathouwer@telenet.be>
2016-05-31 13:22:33 +0200

    added mega

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 # associative array for the platforms that will be verified in build_main_platforms()
 # this will be eval'd in the functions below because arrays can't be exported
-export MAIN_PLATFORMS='declare -A main_platforms=( [uno]="arduino:avr:uno" [esp8266]="esp8266:esp8266:huzzah" )'
+export MAIN_PLATFORMS='declare -A main_platforms=( [uno]="arduino:avr:uno" [mega]="arduino:avr:mega" [esp8266]="esp8266:esp8266:huzzah" )'
 
 # associative array for other platforms that can be called explicitly in .travis.yml configs
 # this will be eval'd in the functions below because arrays can't be exported
