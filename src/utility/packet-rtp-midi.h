1cd02420f281fda2c18ed658c029550da90d65a3
Massimo Fasciano <massimo@mafalitic.com>
2018-04-09 09:45:51 -0400

    fixed pitch bend byte order and offset

--- src/utility/packet-rtp-midi.h
+++ src/utility/packet-rtp-midi.h
@@ -3,7 +3,7 @@
  *  Project		Arduino AppleMIDI Library
  *	@brief		AppleMIDI Library for the Arduino
  *	Version		0.0
- *  @author		lathoub, hackmancoltaire, rolimat
+ *  @author		lathoub, hackmancoltaire, rolimat, massimofasciano
  *	@date		01/04/13
  *  License		Code is open source so please feel free to do anything you want with it; you buy me a beer if you use this and we meet someday (Beerware license).
  */
@@ -1304,7 +1304,7 @@ DEBUGSTREAM.println("aborted MIDI-command 2: pitch_bend_change");
 			return cmd_len;
 		}
 
-		int pitch = ( octet1 << 7 ) | octet2;
+		int pitch = (( octet2 << 7 ) | octet1) - 8192;
 
 		if ( using_rs ) {
 		} else {
