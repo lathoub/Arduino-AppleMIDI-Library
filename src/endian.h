a956613b680927cc61e46a699cda9dab004002b5
lathoub <lathoub@gmail.com>
2019-11-03 01:12:05 +1100

    guards around #define

--- src/endian.h
+++ src/endian.h
@@ -22,12 +22,17 @@
 
 #include <stdint.h>
 
+#ifndef __bswap16
 #define __bswap16(x) ((uint16_t)((((uint16_t)(x)&0xff00) >> 8) | (((uint16_t)(x)&0x00ff) << 8)))
+#endif
 
+#ifndef __bswap32
 #define __bswap32(x)                                                                     \
 	((uint32_t)((((uint32_t)(x)&0xff000000) >> 24) | (((uint32_t)(x)&0x00ff0000) >> 8) | \
 				(((uint32_t)(x)&0x0000ff00) << 8) | (((uint32_t)(x)&0x000000ff) << 24)))
+#endif
 
+#ifndef __bswap64
 #define __bswap64(x)                                            \
 	((uint64_t)((((uint64_t)(x)&0xff00000000000000ULL) >> 56) | \
 				(((uint64_t)(x)&0x00ff000000000000ULL) >> 40) | \
@@ -37,6 +42,7 @@
 				(((uint64_t)(x)&0x0000000000ff0000ULL) << 24) | \
 				(((uint64_t)(x)&0x000000000000ff00ULL) << 40) | \
 				(((uint64_t)(x)&0x00000000000000ffULL) << 56)))
+#endif
 
 union conversionBuffer
 {
