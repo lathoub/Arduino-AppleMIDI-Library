efeebad05113b8384f2a7eaef4bd82a4ebb3b7d8
lathoub <lathoub@gmail.com>
2019-12-30 17:04:02 +0100

    increase buffer size from 64 to 128
    Max message size during testing was 81

--- src/AppleMidi_Settings.h
+++ src/AppleMidi_Settings.h
@@ -6,7 +6,7 @@ BEGIN_APPLEMIDI_NAMESPACE
 
 struct DefaultSettings
 {
-    static const size_t MaxBufferSize = 64;
+    static const size_t MaxBufferSize = 128;
 
     /* The MaxNumberOfParticipants constant is typically the amount
     of sockets in the system divided by 2 */
