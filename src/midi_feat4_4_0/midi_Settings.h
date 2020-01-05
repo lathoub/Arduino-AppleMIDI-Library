290ed7df5edd022981027f00a36b97a9219ee766
lathoub <lathoub@gmail.com>
2020-01-05 10:56:16 +0100

    Update midi_Settings.h
    no running status

--- src/midi_feat4_4_0/midi_Settings.h
+++ src/midi_feat4_4_0/midi_Settings.h
@@ -53,7 +53,7 @@ struct DefaultSettings
     Must be disabled to send USB MIDI messages to a computer
     Warning: does not work with some hardware, enable with caution.
     */
-    static const bool UseRunningStatus = true;
+    static const bool UseRunningStatus = false;
 
     /*! NoteOn with 0 velocity should be handled as NoteOf.\n
     Set to true  to get NoteOff events when receiving null-velocity NoteOn messages.\n
