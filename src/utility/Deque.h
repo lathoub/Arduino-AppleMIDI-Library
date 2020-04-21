42987814c105ed1e4261c3f23825ca37cf1caf1a
lathoub <4082369+lathoub@users.noreply.github.com>
2020-04-21 06:31:23 +0200

    removed unused variable

--- src/utility/Deque.h
+++ src/utility/Deque.h
@@ -138,7 +138,6 @@ template<typename T, size_t Size>
 void Deque<T, Size>::pop_back() {
     if (empty()) // if empty, do nothing.
         return;
-    auto item = front();
     if (--_head < 0)
         _head = Size - 1;
     if (_head == _tail) //now buffer is empty
