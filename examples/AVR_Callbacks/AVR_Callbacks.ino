397eab2afb74b1b1e224b3188e43616cee07eb6a
lathoub <4082369+lathoub@users.noreply.github.com>
2020-12-22 23:05:37 +0100

    renamed EthernetShield to AVR for AVR type Arduino's

---
+++ examples/AVR_Callbacks/AVR_Callbacks.ino
@@ -0,0 +1,134 @@
+#include <Ethernet.h>
+
+#include <AppleMIDI.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+unsigned long t1 = millis();
+bool isConnected = false;
+
+APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void setup()
+{
+  DBG_SETUP(115200);
+  DBG("Booting");
+
+  if (Ethernet.begin(mac) == 0) {
+    DBG(F("Failed DHCP, check network cable & reboot"));
+    for (;;);
+  }
+
+  DBG(F("OK, now make sure you an rtpMIDI session that is Enabled"));
+  DBG(F("Add device named Arduino with Host"), Ethernet.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
+  DBG(F("Select and then press the Connect button"));
+  DBG(F("Then open a MIDI listener and monitor incoming notes"));
+
+  MIDI.begin();
+
+  // Stay informed on connection status
+  AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc, const char* name) {
+    isConnected = true;
+    DBG(F("Connected to session"), name);
+  });
+  AppleMIDI.setHandleDisconnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc) {
+    isConnected = false;
+    DBG(F("Disconnected"));
+  });
+  
+  AppleMIDI.setHandleSendRtp([](const APPLEMIDI_NAMESPACE::Rtp_t & rtp) {
+    DBG(F("setHandleSendRtp"), rtp.sequenceNr);
+  });
+  AppleMIDI.setHandleReceivedRtp([](const APPLEMIDI_NAMESPACE::Rtp_t & rtp, const int32_t& latency) {
+    DBG(F("setHandleReceivedRtp"), rtp.sequenceNr , latency);
+  });
+  AppleMIDI.setHandleStartReceivedMidi([](const APPLEMIDI_NAMESPACE::ssrc_t&) {
+    DBG(F("setHandleStartReceivedMidi"));
+  });
+  AppleMIDI.setHandleReceivedMidi([](const APPLEMIDI_NAMESPACE::ssrc_t&, byte value) {
+    DBG(F("setHandleReceivedMidi"), value);
+  });
+  AppleMIDI.setHandleEndReceivedMidi([](const APPLEMIDI_NAMESPACE::ssrc_t&) {
+    DBG(F("setHandleEndReceivedMidi"));
+  });
+  
+  AppleMIDI.setHandleException(OnAppleMidiException);
+
+  MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOn"), note);
+  });
+  MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOff"), note);
+  });
+
+  DBG(F("Sending MIDI messages every second"));
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void loop()
+{
+  // Listen to incoming notes
+  MIDI.read();
+
+  // send a note every second
+  // (dont cáll delay(1000) as it will stall the pipeline)
+  if (isConnected && (millis() - t1) > 1000)
+  {
+    t1 = millis();
+
+    byte note = random(1, 127);
+    byte velocity = 55;
+    byte channel = 1;
+
+    MIDI.sendNoteOn(note, velocity, channel);
+    //    MIDI.sendNoteOff(note, velocity, channel);
+  }
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void OnAppleMidiException(const APPLEMIDI_NAMESPACE::ssrc_t& ssrc, const APPLEMIDI_NAMESPACE::Exception& e, const int32_t value ) {
+  switch (e)
+  {
+    case APPLEMIDI_NAMESPACE::Exception::BufferFullException:
+      DBG(F("*** BufferFullException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::ParseException:
+      DBG(F("*** ParseException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::TooManyParticipantsException:
+      DBG(F("*** TooManyParticipantsException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::UnexpectedInviteException:
+      DBG(F("*** UnexpectedInviteException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::ParticipantNotFoundException:
+      DBG(F("*** ParticipantNotFoundException"), value);
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::ListenerTimeOutException:
+      DBG(F("*** ListenerTimeOutException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::MaxAttemptsException:
+      DBG(F("*** MaxAttemptsException"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::NoResponseFromConnectionRequestException:
+      DBG(F("***:yyy did't respond to the connection request. Check the address and port, and any firewall or router settings. (time)"));
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::SendPacketsDropped:
+      DBG(F("*** SendPacketsDropped"), value);
+      break;
+    case APPLEMIDI_NAMESPACE::Exception::ReceivedPacketsDropped:
+      DBG(F("*** ReceivedPacketsDropped"), value);
+      break;
+  }
+}
