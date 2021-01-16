30d569ec2e4f55881b3d9d7a17414975870cc9cf
lathoub <4082369+lathoub@users.noreply.github.com>
2021-01-16 14:37:10 +0100

    Create Teensy41_NoteOnOffEverySec.ino

---
+++ examples/Teensy41_NoteOnOffEverySec/Teensy41_NoteOnOffEverySec.ino
@@ -0,0 +1,91 @@
+#include <NativeEthernet.h>
+
+#define SerialMon Serial
+#define APPLEMIDI_DEBUG SerialMon
+#include <AppleMIDI.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+unsigned long t1 = millis();
+int8_t isConnected = 0;
+
+APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void setup()
+{
+  DBG_SETUP(115200);
+  DBG("Booting");
+
+  if (Ethernet.begin(mac) == 0) {
+    DBG(F("Failed DHCP, check network cable & reboot"));
+    for (;;);
+  }
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    DBG("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  while (Ethernet.linkStatus() == LinkOFF) {
+    DBG("Ethernet cable is not connected.");
+    delay(500);
+  }
+  
+  DBG(F("OK, now make sure you an rtpMIDI session that is Enabled"));
+  DBG(F("Add device named Arduino with Host"), Ethernet.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
+  DBG(F("Select and then press the Connect button"));
+  DBG(F("Then open a MIDI listener and monitor incoming notes"));
+
+  MIDI.begin();
+
+  // Stay informed on connection status
+  AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc, const char* name) {
+    isConnected++;
+    DBG(F("Connected to session"), ssrc, name);
+  });
+  AppleMIDI.setHandleDisconnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc) {
+    isConnected--;
+    DBG(F("Disconnected"), ssrc);
+  });
+  
+  MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOn"), note);
+  });
+  MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOff"), note);
+  });
+
+  DBG(F("Sending MIDI messages every second"));
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void loop()
+{
+  // Listen to incoming notes
+  MIDI.read();
+
+  // send a note every second
+  // (dont cáll delay(1000) as it will stall the pipeline)
+  if ((isConnected > 0) && (millis() - t1) > 1000)
+  {
+    t1 = millis();
+
+    byte note = random(1, 127);
+    byte velocity = 55;
+    byte channel = 1;
+
+    MIDI.sendNoteOn(note, velocity, channel);
+//    MIDI.sendNoteOff(note, velocity, channel);
+  }
+}
