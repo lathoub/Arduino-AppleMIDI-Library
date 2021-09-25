364a61bcc673df4fc30de597d2c2768bf4a69cd6
lathoub <bart.de.lathouwer@telenet.be>
2021-09-25 17:30:38 +0200

    make debug MACRO (more) unique

--- src/AppleMIDI_Debug.h
+++ src/AppleMIDI_Debug.h
@@ -1,29 +1,36 @@
+#pragma once
+
+#include "AppleMIDI_Namespace.h"
+BEGIN_APPLEMIDI_NAMESPACE
+
 #ifdef APPLEMIDI_DEBUG
 namespace {
-static void DBG_SETUP(unsigned long baud) {
+static void AM_DBG_SETUP(unsigned long baud) {
   APPLEMIDI_DEBUG.begin(baud);
   while (!APPLEMIDI_DEBUG);
 }
 
 template <typename T>
-static void DBG_PLAIN(T last) {
+static void AM_DBG_PLAIN(T last) {
   APPLEMIDI_DEBUG.println(last);
 }
 
 template <typename T, typename... Args>
-static void DBG_PLAIN(T head, Args... tail) {
+static void AM_DBG_PLAIN(T head, Args... tail) {
   APPLEMIDI_DEBUG.print(head);
   APPLEMIDI_DEBUG.print(' ');
-  DBG_PLAIN(tail...);
+  AM_DBG_PLAIN(tail...);
 }
 
 template <typename... Args>
-static void DBG(Args... args) {
-  DBG_PLAIN(args...);
+static void AM_DBG(Args... args) {
+  AM_DBG_PLAIN(args...);
 }
 }  // namespace
 #else
-#define DBG_SETUP(...)
-#define DBG_PLAIN(...)
-#define DBG(...)
+#define AM_DBG_SETUP(...)
+#define AM_DBG_PLAIN(...)
+#define AM_DBG(...)
 #endif
+
+END_APPLEMIDI_NAMESPACE
